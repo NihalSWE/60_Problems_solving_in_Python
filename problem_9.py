c3bc8238529cf70ebe4ba63dbabedb9a2817b5e9
NihalSWE <mahamudur35-2563@diu.edu.bd>
2024-03-03 04:49:36 +0600

    9th problem

---
+++ problem_9.py
@@ -0,0 +1,9 @@
+# Exercise 9
+# Write a Python program that calculates the average of three integers entered by the user.
+
+m=int(input("Enter first number: "))
+n=int(input("Enter second number: "))
+l=int(input("Enter third number: "))
+
+avg=(m+n+l)/3
+print("The average is ", avg)
