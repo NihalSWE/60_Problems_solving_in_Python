7d3ce8460c39f7ff185bbb343db335d18a754a9a
NihalSWE <mahamudur35-2563@diu.edu.bd>
2024-03-03 04:46:31 +0600

    8th problem

---
+++ problem_8.py
@@ -0,0 +1,13 @@
+# Exercise 8
+                                                                 
+# Write a Python program that calculates the absolute value of an integer entered by the user.
+
+num=int(input("Enter a number : "))
+if num > 0 :
+    print(f"the absolute value of {num} is {abs(num)}")
+elif -num>0:
+    print(f"The absolute value of {num} is {abs(-num)}")
+else:
+    print("The input number is zero.")
+
+    print("Null")
