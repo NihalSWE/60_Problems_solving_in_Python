e1a97cc34a92866c4dbb6d07bc9ced4e9056495a
NihalSWE <mahamudur35-2563@diu.edu.bd>
2024-03-03 03:31:11 +0600

    2nd problem

---
+++ problem_2.py
@@ -0,0 +1,6 @@
+# Exercise 2 Write a Python program to enter two numbers and display their product.  
+
+a=float(input("Enter first number: "))
+b=float(input("Enter second number: "))
+product = a * b
+print(f"The product of a and  b is {product}")
