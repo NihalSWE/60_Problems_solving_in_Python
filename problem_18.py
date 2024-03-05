2298936fb40ef86c27fffba9da1bd1462e3a2cca
NihalSWE <mahamudur35-2563@diu.edu.bd>
2024-03-05 23:27:11 +0600

    18th problem

---
+++ problem_18.py
@@ -0,0 +1,8 @@
+# Exercise 18
+# Write a Python program that calculates the sum S=1+2+3+...+ 10. Using the for loop.   
+
+
+S=0
+for i in range(1,11):
+    S=S+i
+print(f"The sum of 1 to 10 is : {S}")
