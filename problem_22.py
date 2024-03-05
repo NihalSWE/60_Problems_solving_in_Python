b9f1673287f60f45bb7fc07b653299f68ad58894
NihalSWE <mahamudur35-2563@diu.edu.bd>
2024-03-06 03:30:55 +0600

    22th problem

---
+++ problem_22.py
@@ -0,0 +1,17 @@
+# Exercise 22
+# Write a Python program, enter two numbers from the user and find the greatest common divisor using the for loop.
+
+m=int(input("Enter the first number: "))
+n=int(input("Enter the second number: "))
+
+if m<n:
+    min=n
+else:
+    min=m
+
+for i in range (1,min+1):
+    if  m%i==0 and n%i==0:
+        gcd=i
+print(f"The common divisor of {m} and {n} is = {gcd}")
+
+    
