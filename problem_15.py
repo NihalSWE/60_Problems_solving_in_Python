a5411051bcdc459a1176ea425e0f4701290ea260
NihalSWE <mahamudur35-2563@diu.edu.bd>
2024-03-05 23:15:28 +0600

    15th problem

---
+++ problem_15.py
@@ -0,0 +1,10 @@
+# Exercise 15
+# Write a Python program to calculate the sum S= 1+2+3+...+ 10. Using the while loop.  
+
+i=1
+s=0
+while i<=10:
+    s=s+i
+    print(s)
+    i+=1
+print("The sum of 1 to 10 is :",s)
