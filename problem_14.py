60c39b1d64c6ffaae9b68f1c33502b28fb5aa2b2
NihalSWE <mahamudur35-2563@diu.edu.bd>
2024-03-05 23:10:31 +0600

    14th problem

---
+++ problem_14.py
@@ -0,0 +1,12 @@
+# Exercise 14
+# Write a Python program that displays the message “Good evening” 10 times. Using the while loop.   
+
+
+msg= "Good evening"
+
+count=1
+while  count<=10:
+    print(msg,count)
+    count+=1
+
+
